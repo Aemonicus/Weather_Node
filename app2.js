83e6932766f6c2106f556cebe08649d3d7ae3d9b
Florent VICENTE <vicentegroupe@gmail.com>
2019-06-03 17:51:58 +0200

    index fr 2

--- app2.js
+++ app2.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirPath));
 // Setup index page
 app.get("", (req, res) => {
   res.render("index", {
-    title: "Weather App on a Dynamic Site",
+    title: "Weather App",
     name: "Florent VICENTE"
   });
 });
